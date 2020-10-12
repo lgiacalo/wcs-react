37edace314dd9f3152a667505526d9ce5f16cd83
lgiacalo <lgiacalo@student.42.fr>
2020-10-12 19:41:58 +0200

    react02-1: ok

--- simpsons-quotes-react/src/App.js
+++ simpsons-quotes-react/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar'
+import QuoteCard from './components/QuoteCard';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <QuoteCard />
+      <QuoteCard />
     </div>
   );
 }

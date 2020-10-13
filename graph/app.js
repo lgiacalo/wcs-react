16db7e6ff963e2e34ce576b179ac5c002521c834
lgiacalo <lgiacalo@student.42.fr>
2020-10-13 17:18:16 +0200

    react: chap5: wip

--- graph/app.js
+++ graph/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable max-len */
 /* eslint-disable max-classes-per-file */
 /* eslint-disable react/destructuring-assignment */
@@ -10,7 +11,7 @@ React.Component = window.React.Component;
 
 function WelcomeFunc({ name, children }) {
   // console.log('props :>> '); /* Object { name: "Jean" } */
-  console.log('children :>> ', children);
+  // console.log('children :>> ', children);
   return (
     <div>
       <h1>Bnojour {name}</h1>
@@ -22,11 +23,11 @@ function WelcomeFunc({ name, children }) {
 class Welcome extends React.Component {
   constructor(props) {
     super(props);
-    console.log('props :>> ', props);
+    // console.log('props :>> ', props);
   }
 
   render() {
-    console.log('this.props :>> ', this.props);
+    // console.log('this.props :>> ', this.props);
     return (
       <div>
         <h1>Bnojour {this.props.name}</h1>
@@ -72,11 +73,37 @@ class Clock extends React.Component {
   }
 }
 
+class ManualIncrementer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { n: 0 };
+  }
+
+  increment(e) {
+    e.preventDefault();
+    this.setState((state, props) => ({ n: state.n + 1 }));
+  }
+
+  render() {
+    return (
+      <div>
+        Valeur: {this.state.n}
+        <button onClick={this.increment.bind(this)} type="button">
+          Incrementer
+        </button>
+      </div>
+    );
+  }
+}
+
 class Incrementer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { n: props.start, step: props.step };
-    this.timer = null;
+    this.state = {
+      n: props.start,
+      step: props.step,
+      timer: null,
+    };
   }
 
   componentDidMount() {
@@ -86,11 +113,12 @@ class Incrementer extends React.Component {
     //     n: number,
     //   });
     // }, 1000);
-    this.timer = window.setInterval(this.increment.bind(this), 1000);
+    // this.timer = window.setInterval(this.increment.bind(this), 1000);
+    this.play();
   }
 
   componentWillUnmount() {
-    window.clearInterval(this.timer);
+    window.clearInterval(this.state.timer);
   }
 
   increment() {
@@ -102,14 +130,46 @@ class Incrementer extends React.Component {
     this.setState((state, props) => ({ n: state.n + props.step }));
   }
 
+  pause() {
+    window.clearInterval(this.state.timer);
+    this.setState({ timer: null });
+  }
+
+  play() {
+    window.clearInterval(this.state.timer);
+    this.setState({
+      timer: window.setInterval(this.increment.bind(this), 1000),
+    });
+  }
+
+  toggle() {
+    return this.state.timer ? this.pause() : this.play();
+  }
+
+  reset() {
+    this.setState({ n: 0 });
+  }
+
   render() {
-    return <div>{this.state.n}</div>;
+    const method = this.state.timer
+      ? this.pause.bind(this)
+      : this.play.bind(this);
+    const label = this.state.timer ? 'pause' : 'play';
+
+    return (
+      <div>
+        Valeur: {this.state.n}
+        <button onClick={this.toggle.bind(this)}>{label}</button>
+        <button onClick={this.reset.bind(this)}>Reset</button>
+      </div>
+    );
   }
 }
 
 Incrementer.defaultProps = {
   start: 0,
   step: 1,
+  pause: false,
 };
 
 function Home() {
@@ -118,10 +178,37 @@ function Home() {
       <Welcome name="Dorothée" />
       <Welcome name="Jean" />
       <Clock />
-      <Incrementer start={10} />
-      <Incrementer start={10} step={10} />
+      {/* <ManualIncrementer /> */}
+      <Incrementer />
     </div>
   );
 }
 
 ReactDOM.render(<Home />, document.querySelector('#app'));
+
+
+
+
+
+
+
+
+ // toggleButton() {
+  //   console.log('this.state :>> ', this.state);
+  //   if (this.state.pause) {
+  //     this.timer = window.setInterval(this.increment.bind(this), 1000);
+  //     this.setState((state) => ({ pause: false, ...state }));
+  //     console.log('IF this.state :>> ', this.state);
+  //     console.log('IF this.timer :>> ', this.timer);
+
+  //   }
+  //   else {
+  //     console.log('ElSE this.timer :>> ', this.timer);
+
+  //     window.clearInterval(this.timer);
+  //     this.setState((state) => ({ pause: true, ...state }));
+  //     console.log('ELSE this.state :>> ', this.state);
+
+  //   }
+  //   console.log('this.state end :>> ', this.state);
+  // }

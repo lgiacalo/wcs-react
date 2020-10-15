8db5603849a46eb0d7646fac1e3de0fedd94b05b
lgiacalo <lgiacalo@student.42.fr>
2020-10-15 19:37:09 +0200

    react10: bof bof

---
+++ demo-app-router/src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import PostList from './components/PostList';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/2019/09">September 2019</Link>
+            </li>
+            <li>
+              <Link to="/2019/10">October 2019</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/:year/:month" component={PostList} />
+          {/* <Route path="/:year/:month" >
+            <PostList />
+          </Route> */}
+
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+

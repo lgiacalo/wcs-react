70298ed1293bbc83f0a96b8661ffa737ddcceb24
lgiacalo <lgiacalo@student.42.fr>
2020-10-15 12:36:06 +0200

    react07: challenge ok

--- simpsons-quotes-react/src/App.js
+++ simpsons-quotes-react/src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
+import axios from 'axios';
+import './App.css';
+
 import Navbar from './components/Navbar'
 import QuoteCard from './components/QuoteCard';
 
+const initSimpson = {
+  quote: "Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?",
+  character: "Abe Simpson",
+  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593",
+}
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
+const urls = [
+  "https://thesimpsonsquoteapi.glitch.me/quotes",
+  "https://simpsons-quotes-api.herokuapp.com/quotes"
+]
 
-      <QuoteCard
-        quote={"Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?"}
-        character="Abe Simpson"
-        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593"
-      />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initSimpson;
+  };
 
-      <QuoteCard
-        quote={"I believe the children are the future... Unless we stop them now!"}
-        character="Homer Simpson"
-        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-      />
-    </div>
-  );
+  async getSimpson(url) {
+    return await axios.get(url)
+      .then(response => response.data[0])
+      .catch(error => error.response);
+  }
+
+  reloadQuote = async () => {
+    let newSimpson = await this.getSimpson(urls[0]);
+
+    if (!newSimpson || newSimpson.status === 404)
+      newSimpson = await this.getSimpson(urls[1]);
+
+    this.setState({
+      quote: newSimpson.quote,
+      character: newSimpson.character,
+      image: newSimpson.image
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+
+        <button type="button" onClick={this.reloadQuote}>Reload for another quote!</button>
+        <QuoteCard
+          quote={this.state.quote}
+          character={this.state.character}
+          image={this.state.image}
+        />
+
+      </div>
+    );
+  }
 }
 
 export default App;

13b7133b8c2bff7c2bd76a02687a75ca8f7c5d4e
lgiacalo <lgiacalo@student.42.fr>
2020-11-03 23:52:36 +0100

    add DELETE_TODO_ACTION

--- react-redux/src/components/TodoList.jsx
+++ react-redux/src/components/TodoList.jsx
@@ -1,9 +1,9 @@
 import { useCallback } from "react";
 import { connect, useSelector, useDispatch } from "react-redux";
 import { todosSelector } from "../store/todosSelectors";
-import { toggleTodoAction } from "../store/todosActions";
+import { deleteTodoAction, toggleTodoAction } from "../store/todosActions";
 
-function TodoItem({ todo, onToggle }) {
+function TodoItem({ todo, onToggle, onDelete }) {
   return (
     <li>
       <label>
@@ -13,16 +13,22 @@ function TodoItem({ todo, onToggle }) {
           onChange={() => onToggle(todo)}
         />
         {todo.title}
+        <button onClick={() => onDelete(todo)}>x</button>
       </label>
     </li>
   );
 }
 
-export function TodoList({ todos, onToggle }) {
+export function TodoList({ todos, onToggle, onDelete }) {
   return (
     <ul>
       {todos.map((todo) => (
-        <TodoItem todo={todo} onToggle={onToggle} key={todo.id} />
+        <TodoItem
+          todo={todo}
+          onToggle={onToggle}
+          key={todo.id}
+          onDelete={onDelete}
+        />
       ))}
     </ul>
   );
@@ -37,8 +43,14 @@ export function TodoListStore() {
     },
     [dispatch]
   );
+  const onDelete = useCallback(
+    (todo) => {
+      dispatch(deleteTodoAction(todo));
+    },
+    [dispatch]
+  );
 
-  return <TodoList todos={todos} onToggle={onToggle} />;
+  return <TodoList todos={todos} onToggle={onToggle} onDelete={onDelete} />;
 }
 
 // export const TodoListStore = connect(
--- react-redux/src/store/todosReducer.js
+++ react-redux/src/store/todosReducer.js
@@ -14,6 +14,7 @@ const initialState = [
 
 export const ADD_TODO_ACTION = "ADD_TODO_ACTION";
 export const UPDATE_TODO_ACTION = "UPDATE_TODO_ACTION";
+export const DELETE_TODO_ACTION = "DELETE_TODO_ACTION";
 
 export function todosReducer(state = initialState, action) {
   switch (action.type) {
@@ -25,6 +26,8 @@ export function todosReducer(state = initialState, action) {
           return { ...todo, ...action.payload };
         return todo;
       });
+    case DELETE_TODO_ACTION:
+      return state.filter((todo) => todo.id !== action.payload);
     default:
       return state;
   }
